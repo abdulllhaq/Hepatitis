c949fcdd939f19fc4bd3d7d9942be62e4e6d427c
braxtonova <85130590+braxtonova@users.noreply.github.com>
2021-06-16 23:45:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -45,29 +45,32 @@ x_train, x_test, y_train, y_test = train_test_split(x,y, test_size = 0.2, random
 
 #user report
 def user_report():
-  sex = st.sidebar.selectbox('Sex',('male','female'))
-  age = st.sidebar.slider('Age', 0,200, 75 )
-  trestbps = st.sidebar.slider('Resting Blood Pressure', 60,200, 126 )
-  chol = st.sidebar.slider('Cholestrol', 100,500, 330 )
-  thalach = st.sidebar.slider('Maximum Heart Rate Achieved', 60,250, 146 )
-  oldpeak = st.sidebar.slider('ST Depression Induced by Exercise', 0.0,5.0, 2.50 )
-  ca = st.sidebar.slider('Number of major vessels coloured by Flouroscopy', 0,5, 2 )
-  
-  sex_n = 1
-  if sex=='male': 
-    sex_n = 1
-  else: sex_n=0
+  Sex = st.sidebar.selectbox('Sex',('Male','Female'))
+  Age = st.sidebar.slider('Age', 0,200, 75 )
+  Albumin = st.sidebar.slider('Albumin', 60,200, 126 )
+  Alkaline Phosphate = st.sidebar.slider('Alkaline Phosphate', 100,500, 330 )
+  Alanine Aminotransferase = st.sidebar.slider('Alanine Aminotransferase', 60,250, 146 )
+  Aspartate Aminotransferase = st.sidebar.slider('Aspartate Aminotransferase', 0.0,5.0, 2.50 )
+  Bilirubin = st.sidebar.slider('Bilirubin', 0,5, 2 )
+  Serum Cholinesterase = st.sidebar.slider('Serum Cholinesterase', 1,50, 12)
+  Cholestrol = st.sidebar.slider('Cholestrol', 1,12, 3)
+  Creatinine = st.sidebar.slider('Creatinine', 12,120, 75)
+  Gamma-Glutamyl Transferase = st.sidebar.slider('Gamma-Glutamyl Transferase', 10,200, 120)
+  Prothrombin = st.sidebar.slider('Prothrombin', 25,120, 75)
 
   user_report_data = {
-      'age':age,
-      'trestbps':trestbps,
-      'chol':chol,
-      'thalach':thalach,
-      'oldpeak':oldpeak,
-      'ca':ca,
-      'sex': sex_n,
-    
-         
+      'Sex':Sex,
+      'Age':Age,
+      'Albumin':Albumin,
+      'Alkaline Phosphate':Alkaline Phosphate,
+      'Alanine Aminotransferase':Alanine Aminotransferase,
+      'Aspartate Aminotransferase':Aspartate Aminotransferase,
+      'Bilirubin':Bilirubin,
+      'Serum Cholinesterase':Serum Cholinesterase,
+      'Cholestrol':Cholestrol,
+      'Creatinine':Creatinine,
+      'Gamma-Glutamyl Transferase':Gamma-Glutamyl Transferase,
+      'Prothrombin':Prothrombin,  
   }
   report_data = pd.DataFrame(user_report_data, index=[0])
   return report_data
@@ -103,58 +106,102 @@ else:
 
 
 #rbp
-st.header('Resting Blood Pressure Value Graph (Yours vs Others)')
-fig_trestbps = plt.figure()
-ax3 = sns.scatterplot(x = 'Age', y = 'Resting Blood Pressure', data = df, hue = 'Outcome' , palette='Purples')
-ax4 = sns.scatterplot(x = user_data['age'], y = user_data['trestbps'], s = 150, color = color)
+st.header('Albumin Value Graph (Yours vs Others)')
+fig_Albumin = plt.figure()
+ax3 = sns.scatterplot(x = 'Age', y = 'Albumin', data = df, hue = 'Outcome' , palette='Purples')
+ax4 = sns.scatterplot(x = user_data['age'], y = user_data['Albumin'], s = 150, color = color)
 plt.xticks(np.arange(0,100,5))
 plt.yticks(np.arange(60,220,10))
-plt.title('0 - Healthy & 1 - Unhealthy')
-st.pyplot(fig_trestbps)
+plt.title('0 - Healthy, 1 - Hepatitis, 2 - Fibrosis, 3 - Cirrhosis')
+st.pyplot(fig_Albumin)
 
 
 #chol, 2013 damn it
-st.header('Cholestrol Value Graph (Yours vs Others)')
-fig_i = plt.figure()
-ax9 = sns.scatterplot(x = 'Age', y = 'Cholestrol', data = df, hue = 'Outcome', palette='rainbow')
-ax10 = sns.scatterplot(x = user_data['age'], y = user_data['chol'], s = 150, color = color)
+st.header('Alkaline Phosphate Value Graph (Yours vs Others)')
+fig_Alkaline Phosphate = plt.figure()
+ax9 = sns.scatterplot(x = 'Age', y = 'Alkaline Phosphate', data = df, hue = 'Outcome', palette='rainbow')
+ax10 = sns.scatterplot(x = user_data['age'], y = user_data['Alkaline Phosphate'], s = 150, color = color)
 plt.xticks(np.arange(0,100,5))
 plt.yticks(np.arange(100,750,50))
-plt.title('0 - Healthy & 1 - Unhealthy')
-st.pyplot(fig_i)
+plt.title('0 - Healthy, 1 - Hepatitis, 2 - Fibrosis, 3 - Cirrhosis')
+st.pyplot(fig_Alkaline Phosphate)
 
 #Hmax
-st.header('Maximum Heart Rate Achieved Value Graph (Yours vs Others)')
-fig_thalach = plt.figure()
-ax5 = sns.scatterplot(x = 'Age', y = 'Maximum Heart Rate Achieved', data = df, hue = 'Outcome', palette='Blues')
-ax6 = sns.scatterplot(x = user_data['age'], y = user_data['thalach'], s = 150, color = color)
+st.header('Alanine Aminotransferase Value Graph (Yours vs Others)')
+fig_Alanine Aminotransferase = plt.figure()
+ax5 = sns.scatterplot(x = 'Age', y = 'Alanine Aminotransferase', data = df, hue = 'Outcome', palette='Blues')
+ax6 = sns.scatterplot(x = user_data['age'], y = user_data['Alanine Aminotransferase'], s = 150, color = color)
 plt.xticks(np.arange(0,100,5))
 plt.yticks(np.arange(40,250,25))
-plt.title('0 - Healthy & 1 - Unhealthy')
-st.pyplot(fig_thalach)
+plt.title('0 - Healthy, 1 - Hepatitis, 2 - Fibrosis, 3 - Cirrhosis')
+st.pyplot(fig_Alanine Aminotransferase)
 
 
 #STDIE
-st.header('ST Depression Induced by Exercise Value Graph (Yours vs Others)')
-fig_oldpeak = plt.figure()
-ax11 = sns.scatterplot(x = 'Age', y = 'ST Depression Induced by Exercise', data = df, hue = 'Outcome', palette='Greens')
-ax12 = sns.scatterplot(x = user_data['age'], y = user_data['oldpeak'], s = 150, color = color)
+st.header('Aspartate Aminotransferase Value Graph (Yours vs Others)')
+fig_Aspartate Aminotransferase = plt.figure()
+ax11 = sns.scatterplot(x = 'Age', y = 'Aspartate Aminotransferase', data = df, hue = 'Outcome', palette='Greens')
+ax12 = sns.scatterplot(x = user_data['age'], y = user_data['Aspartate Aminotransferase'], s = 150, color = color)
 plt.xticks(np.arange(0,100,5))
 plt.yticks(np.arange(0,8,0.5))
-plt.title('0 - Healthy & 1 - Unhealthy')
-st.pyplot(fig_oldpeak)
+plt.title('0 - Healthy, 1 - Hepatitis, 2 - Fibrosis, 3 - Cirrhosis')
+st.pyplot(fig_Aspartate Aminotransferase)
 
 
 #FCV
-st.header('Number of major vessels coloured by Flouroscopy Value Graph (Yours vs Others)')
-fig_ca = plt.figure()
-ax13 = sns.scatterplot(x = 'Age', y = 'Number of major vessels coloured by Flouroscopy', data = df, hue = 'Outcome', palette='rocket')
-ax14 = sns.scatterplot(x = user_data['age'], y = user_data['ca'], s = 150, color = color)
+st.header('Bilirubin Value Graph (Yours vs Others)')
+fig_Bilirubin = plt.figure()
+ax13 = sns.scatterplot(x = 'Age', y = 'Bilirubin', data = df, hue = 'Outcome', palette='rocket')
+ax14 = sns.scatterplot(x = user_data['age'], y = user_data['Bilirubin'], s = 150, color = color)
 plt.xticks(np.arange(0,100,5))
 plt.yticks(np.arange(0,5,1))
-plt.title('0 - Healthy & 1 - Unhealthy')
-st.pyplot(fig_ca)
+plt.title('0 - Healthy, 1 - Hepatitis, 2 - Fibrosis, 3 - Cirrhosis')
+st.pyplot(fig_Bilirubin)
 
+st.header('Serum Cholinesterase	 Value Graph (Yours vs Others)')
+fig_Serum Cholinesterase = plt.figure()
+ax13 = sns.scatterplot(x = 'Age', y = 'Serum Cholinesterase	', data = df, hue = 'Outcome', palette='rocket')
+ax14 = sns.scatterplot(x = user_data['age'], y = user_data['Serum Cholinesterase'], s = 150, color = color)
+plt.xticks(np.arange(0,100,5))
+plt.yticks(np.arange(0,5,1))
+plt.title('0 - Healthy, 1 - Hepatitis, 2 - Fibrosis, 3 - Cirrhosis')
+st.pyplot(fig_Serum Cholinesterase)
+
+st.header('Cholestrol Value Graph (Yours vs Others)')
+fig_Cholestrol = plt.figure()
+ax13 = sns.scatterplot(x = 'Age', y = 'Cholestrol', data = df, hue = 'Outcome', palette='rocket')
+ax14 = sns.scatterplot(x = user_data['age'], y = user_data['Cholestrol'], s = 150, color = color)
+plt.xticks(np.arange(0,100,5))
+plt.yticks(np.arange(0,5,1))
+plt.title('0 - Healthy, 1 - Hepatitis, 2 - Fibrosis, 3 - Cirrhosis')
+st.pyplot(fig_Cholestrol)
+
+st.header('Creatinine Value Graph (Yours vs Others)')
+fig_Creatinine = plt.figure()
+ax13 = sns.scatterplot(x = 'Age', y = 'Creatinine', data = df, hue = 'Outcome', palette='rocket')
+ax14 = sns.scatterplot(x = user_data['age'], y = user_data['Creatinine'], s = 150, color = color)
+plt.xticks(np.arange(0,100,5))
+plt.yticks(np.arange(0,5,1))
+plt.title('0 - Healthy, 1 - Hepatitis, 2 - Fibrosis, 3 - Cirrhosis')
+st.pyplot(fig_Creatinine)
+
+st.header('Gamma-Glutamyl Transferase Value Graph (Yours vs Others)')
+fig_Gamma-Glutamyl Transferase = plt.figure()
+ax13 = sns.scatterplot(x = 'Age', y = 'Gamma-Glutamyl Transferase', data = df, hue = 'Outcome', palette='rocket')
+ax14 = sns.scatterplot(x = user_data['age'], y = user_data['Bilirubin'], s = 150, color = color)
+plt.xticks(np.arange(0,100,5))
+plt.yticks(np.arange(0,5,1))
+plt.title('0 - Healthy, 1 - Hepatitis, 2 - Fibrosis, 3 - Cirrhosis')
+st.pyplot(fig_Gamma-Glutamyl Transferase)
+
+st.header('Prothrombin Value Graph (Yours vs Others)')
+fig_Prothrombin = plt.figure()
+ax13 = sns.scatterplot(x = 'Age', y = 'Prothrombin', data = df, hue = 'Outcome', palette='rocket')
+ax14 = sns.scatterplot(x = user_data['age'], y = user_data['Bilirubin'], s = 150, color = color)
+plt.xticks(np.arange(0,100,5))
+plt.yticks(np.arange(0,5,1))
+plt.title('0 - Healthy, 1 - Hepatitis, 2 - Fibrosis, 3 - Cirrhosis')
+st.pyplot(fig_Prothrombin)
 
 
 
@@ -163,9 +210,13 @@ st.pyplot(fig_ca)
 st.subheader('Your Report: ')
 output=''
 if user_result[0]==0:
-  output = 'Congratulations, you do not have any heart diseases.'
+  output = 'Congratulations, you do not have any liver diseases.'
+elif user_result[0]==1:
+  output = "Unfortunately, you do have Hepatitis."
+elif user_result[0]==2:
+  output = "Unfortunately, you do have Fibrosis."
 else:
-  output = 'Unfortunately, it is likely that you may be having a heart disease.'
+  output = 'Unfortunately, you do have Cirrosis.'
 st.title(output)
 
 
