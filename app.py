f955c42a0d53dc23b4052a8e5978b94107614c31
braxtonova <85130590+braxtonova@users.noreply.github.com>
2021-06-17 13:30:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -213,7 +213,7 @@ st.pyplot(fig_Prothrombin)
 
 #Final Report
 st.subheader('Your Report: ')
-st.title(user_result[1])
+st.title(user_result[2])
 
 #if user_result[0]==0:
   #output = 'Congratulations, you do not have any liver diseases.'
